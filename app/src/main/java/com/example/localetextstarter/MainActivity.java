63f194211b8a18199bd291c8e6adb3dd3c145e43
Ninissdyah <ninis.dyah1@gmail.com>
2023-04-02 15:47:59 +0700

    baru

--- app/src/main/java/com/example/localetextstarter/MainActivity.java
+++ app/src/main/java/com/example/localetextstarter/MainActivity.java
@@ -9,10 +9,20 @@ import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView inputText;
+    EditText price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 showHelp();
+            }
+        });
+
+        Date myDate = new Date();
+        //durasi waktu untuk menentukan expired dari today
+        long expirationDate = myDate.getTime() + TimeUnit.DAYS.toMillis(5);
+        myDate.setTime(expirationDate);
+
+        String formatDate = DateFormat.getDateInstance().format(myDate);
+        TextView expiredTextview = findViewById(R.id.date);
+        expiredTextview.setText(formatDate);
+
+        inputText = findViewById(R.id.input_text);
+        price = findViewById(R.id.total_price);
+
+        findViewById(R.id.btn_submit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //ubah int => text menggunakan parseInt
+                int harga = Integer.parseInt(inputText.getText().toString());
+                //hitung harga total
+                int totalHarga = harga*100;
+
+                //format total harga
+                String format = NumberFormat.getCurrencyInstance().format(totalHarga);
+                price.setText(format);
             }
         });
     }
@@ -64,6 +100,12 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_help) {
             Intent intent = new Intent(this, HelpActivity.class);
             startActivity(intent);
+            return true;
+        }
+        //untuk memunculkan menu language
+        else if (id ==R.id.action_language) {
+            Intent languageIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(languageIntent);
             return true;
         }
         return super.onOptionsItemSelected(item);
